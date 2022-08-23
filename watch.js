dcec26dcce69ce04288bb21b77e0f133124635b7
Khashayar Mirsadeghi <mms.khashayar@gmail.com>
2022-08-24 01:07:58 +0430

    chore: initial project setup

---
+++ watch.js
@@ -0,0 +1,56 @@
+import { watch } from 'chokidar';
+import { exec } from 'child_process';
+
+const supportedFormats = {
+    html: 'html',
+    sass: ['sass', 'scss'],
+    ts: 'ts'
+};
+
+/**
+ * @typedef {keyof typeof supportedFormats | undefined} SourceCodeChangeType
+ */
+
+/**
+ * @param {String} path
+ *
+ * @returns {SourceCodeChangeType}
+ */
+function getChangeType(path) {
+    for (const [type, mappings] of Object.entries(supportedFormats)) {
+        let extensions = mappings;
+        if (!Array.isArray(mappings)) {
+            extensions = [mappings];
+        }
+
+        for (const extension of extensions) {
+            if (path.endsWith(extension)) {
+                return type;
+            }
+        }
+    }
+
+    return undefined;
+}
+
+watch('./src/**/*', {
+    ignoreInitial: true
+}).on(
+    'all',
+    // eslint-disable-next-line complexity
+    function(_, path) {
+        const changeType = getChangeType(path);
+
+        if (changeType === undefined) {
+            return;
+        }
+
+        const npmCommand = `dev:${changeType}`;
+
+        exec(`npm run ${npmCommand}`, function(error) {
+            if (error) {
+                console.error(error);
+            }
+        });
+    }
+);
