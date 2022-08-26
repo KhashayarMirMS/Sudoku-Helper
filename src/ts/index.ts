82a9d89e0bc028213a3572e3863fb1a44f2b2647
Khashayar Mirsadeghi <mms.khashayar@gmail.com>
2022-08-27 00:31:50 +0430

    feat: add game ui

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,12 @@
-import 'pages/newPuzzle';
-import 'pages/selection';
+import initializeGame from 'pages/game';
+import initializeNewPuzzle from 'pages/new-puzzle';
+import initializeSelection from 'pages/selection';
+import initializeTemplates from 'utils/template';
+import initializeButtons from 'components/button';
+
+initializeButtons();
+initializeTemplates();
+
+initializeNewPuzzle();
+initializeSelection();
+initializeGame();
